1da8f131c337ff75b3490dc97a8604e91cd07521
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-02-19 11:32:59 +0900

    :arrow_up: go@1.6

--- server/build.sh
+++ server/build.sh
@@ -5,7 +5,7 @@ BUILD_VERSION=1.0.0
 BUILD_DATE=`date +%FT%T%z`
 
 SRC_DIR=.
-OUT_DIR=build
+OUT_DIR=bin
 
 mkdir $OUT_DIR
 export GOOS=darwin;export GOARCH=amd64;go build -ldflags "-X main.BuildVersion=$BUILD_VERSION -X main.BuildDate=$BUILD_DATE" -o ${OUT_DIR}/${GOOS}_${GOARCH}/main ${SRC_DIR}/main.go
